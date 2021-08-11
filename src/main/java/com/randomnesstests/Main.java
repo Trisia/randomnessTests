0d717773088af24f065f0c64348b3733e5f87c54
cliven <quanguanyu@qq.com>
2021-08-11 19:44:13 +0800

    通过文件读取

--- src/main/java/com/randomnesstests/Main.java
+++ src/main/java/com/randomnesstests/Main.java
@@ -2,37 +2,61 @@ package com.randomnesstests;
 
 import com.randomnesstests.Tests.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static List<Boolean> B2bit(byte b) {
+        ArrayList<Boolean> arr = new ArrayList<>(8);
+        arr.add((b & 0x80) > 0);
+        arr.add((b & 0x40) > 0);
+        arr.add((b & 0x20) > 0);
+        arr.add((b & 0x10) > 0);
 
-    public static void main(String[] args) {
+        arr.add((b & 0x08) > 0);
+        arr.add((b & 0x04) > 0);
+        arr.add((b & 0x02) > 0);
+        arr.add((b & 0x01) > 0);
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+
         List<Boolean> bits = new ArrayList<>(1500000);
-        for (int i = 0; i < 1000000; i++) {
-            if (Math.random() < 0.5) {
-                bits.add(true);
+//        for (int i = 0; i < 1000000; i++) {
+//            if (Math.random() < 0.5) {
+//                bits.add(true);
+//
+//            } else {
+//                bits.add(false);
+//
+//            }
+//        }
 
-            } else {
-                bits.add(false);
-
-            }
+        final byte[] bytes = Files.readAllBytes(Paths.get("data.bin"));
+        for (byte aByte : bytes) {
+            bits.addAll(B2bit(aByte));
         }
-        System.out.println(MonobitFrequencyTest.run(bits));
-        System.out.println(FrequencyTestWithinABlock.run(bits));
-        System.out.println(PokerTest.run(bits));
-        System.out.println(SerialTest.run(bits));
-        System.out.println(SerialTest.run2(bits));
-        System.out.println(RunsTest.run(bits)); // 分布相关，概率无关
-        System.out.println(RunsDistributionTest.run(bits));
-        System.out.println(testForTheLongestRunOfOnesInABlock.run(bits));
-        System.out.println(BinaryDerivativeTest.run(bits));// 分布相关，概率无关
-        System.out.println(AutocorrelationTest.run(bits));
-        System.out.println(BinaryMatrixRankTest.run(bits));// 分布强相关，概率弱相关
-        System.out.println(CumulativeTest.run(bits));
-        System.out.println(ApproximateEntropyTest.run(bits));
-        System.out.println(LinearComplexityTest.run(bits));// 分布相关，概率无关
-        System.out.println(MaurersUniversalTest.run(bits));
-        System.out.println(DiscreteFourierTransformTest.run(bits));
+
+
+//          System.out.printf("%.6f\n",MonobitFrequencyTest.run(bits));
+        System.out.printf("%.6f\n", FrequencyTestWithinABlock.run(bits));
+        System.out.printf("%.6f\n", PokerTest.run(bits));
+//          System.out.printf("%.6f\n",SerialTest.run(bits));
+//          System.out.printf("%.6f\n",SerialTest.run2(bits));
+//          System.out.printf("%.6f\n",RunsTest.run(bits)); // 分布相关，概率无关
+//          System.out.printf("%.6f\n",RunsDistributionTest.run(bits));
+//          System.out.printf("%.6f\n",testForTheLongestRunOfOnesInABlock.run(bits));
+//          System.out.printf("%.6f\n",BinaryDerivativeTest.run(bits));// 分布相关，概率无关
+//          System.out.printf("%.6f\n",AutocorrelationTest.run(bits));
+//          System.out.printf("%.6f\n",BinaryMatrixRankTest.run(bits));// 分布强相关，概率弱相关
+//          System.out.printf("%.6f\n",CumulativeTest.run(bits));
+//          System.out.printf("%.6f\n",ApproximateEntropyTest.run(bits));
+//          System.out.printf("%.6f\n",LinearComplexityTest.run(bits));// 分布相关，概率无关
+//          System.out.printf("%.6f\n",MaurersUniversalTest.run(bits));
+//          System.out.printf("%.6f\n",DiscreteFourierTransformTest.run(bits));
     }
 }
